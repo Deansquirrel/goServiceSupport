7ac7c86b1ae1a2e0d195d3da77ba7f08f79b640c
Deansquirrel <yuansong6@163.com>
2019-08-12 16:39:09 +0800

    1.0.4增加任务错误记录接口

--- global/Global.go
+++ global/Global.go
@@ -8,7 +8,7 @@ import (
 const (
 	//PreVersion = "1.0.3 Build20190808"
 	//TestVersion = "0.0.0 Build20190101"
-	Version                   = "0.0.0 Build20190101"
+	Version                   = "1.0.4 Build20190808"
 	Type                      = "ServiceSupport"
 	SecretKey                 = "ServiceSupport"
 	ClearJobRecordCron        = "0 0 * * * ?"
